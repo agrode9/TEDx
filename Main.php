223910a7a44796f8c3ff1c6c5ee0689c222273eb
agrode9 <grode2@wisc.edu>
2016-10-26 15:42:45 -0500

    added info to edit later

--- Main.php
+++ Main.php
@@ -14,8 +14,20 @@
     <img src="hero.jpg">
     <h1>TEDxUWMadison</h1>
 </div>
-
+link to tedx https://www.ted.com/about/programs-initiatives/tedx-program
 <body>
+What is TEDx?
+
+In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program of local, 
+self-organized events that bring people together to share a TED-like experience. Our event is called TEDxUWMadison,
+where x = independently organized TED event. At our TEDx[name] event, TEDTalks video and live speakers will 
+combine to spark deep discussion and connection in a small group. The TED Conference provides general guidance
+for the TEDx program, but individual TEDx events, including ours, are self-organized.
+
+
+
+
+
 </body>
 <footer>
   <div id="footer">
