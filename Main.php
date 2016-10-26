c2c4f41b28909a3347e4fe614db733e3a7624211
agrode9 <grode2@wisc.edu>
2016-10-26 14:34:52 -0500

    First commit.

---
+++ Main.php
@@ -0,0 +1,45 @@
+<html>
+  <link href="Format.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Anivers">
+<head>
+    <div id="nav">
+<ul>
+  <div id="navLeft"><li><a href="main.php">Aaron Grode</a></li></div>
+  <li><a href="Contact.php">Contact</a></li>
+  <li><a href="About.php">About</a></li>
+  <li><a href="Resume.php">Resume</a></li>
+</ul>
+</div>
+<div id="background">
+    <img src="guitar.jpg" class="stretch" alt="" />
+</div>
+ <div id="title">
+  <br>
+    About TEDx - UW Madison
+</head>
+<body>
+</body>
+<footer>
+  <div id="footer">
+      <div class="twoColumnFlexVertical">
+        <div class="columnOne">
+          <br>
+          <a href="https://www.linkedin.com/in/AaronGrode"><img src="linkedin.png" style="height:45px;"></a>
+          <a href="mailto:grode2@wisc.edu?Subject=Questions!" target="_top"><img src="mail.png" style="height:45px;"></a>
+          <a href="https://github.com/agrode9"><img src="github.png"  style="height:45px;"></a>
+          <div class="footerList">
+            .                        <a href="Resume.php">Resume</a>                        .
+            <a href="About.php">About</a>                         .
+            <a href="Contact.php">Contact</a>                        .
+          </div>
+        </div>
+        <div class="columnTwo">
+          <div class="footerList">
+            <a href="main.php">Aaron Grode</a>
+          </div>
+        </div>
+      </div>
+  </div>
+</footer>
+</html>
